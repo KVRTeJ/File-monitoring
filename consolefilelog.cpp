4440e3185102a50daa015beeefedc83c8d09a690
KVRTeJ <bb.sssss@inbox.ru>
2025-03-06 22:08:28 +0700

    Создан файл ConsoleFileLog, который является наследником IFileLog. Он выполняет логирование в консоль. Добавлена реализация методов fileExist, fileChanged, fileNotExist

---
+++ consolefilelog.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "consolefilelog.h"
+
+void ConsoleFileLog::fileExist(const QString &path, const qint64 size) {
+    std::cout << "File " << path.toStdString() << " changed size to " << size << std::endl;
+}
+
+void ConsoleFileLog::fileChanged(const QString &path, const qint64 size) {
+    std::cout << "File " << path.toStdString() << " exists and it's size is " << size << std::endl;
+}
+
+void ConsoleFileLog::fileNotExist(const QString &path) {
+    std::cout << "File " << path.toStdString() << " does not exist" << std::endl;
+}
