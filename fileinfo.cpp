80278d6140181353bcdd538334ef5574ac632004
KVRTeJ <bb.sssss@inbox.ru>
2025-03-12 02:09:35 +0700

    Реализованы методы getCondition и update. Также добавлено свойство m_condition, типа Condition, оно хранит текущее состояние файла. Добавлено свойство m_newFileInfo, типа QFileInfo, оно нужно для получения текущей информации о файле. Метод update реализован аналогично методу FileObserber::_listLoop(), он смотрит новые состояния файлай и, в случае изменения состояния, записывает новое состояние(и размер, при необходимости), и возвращает true

--- fileinfo.cpp
+++ fileinfo.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-
 #include "fileinfo.h"
 
 FileInfo::FileInfo(const QString &path)
     : m_path(path)
-{}
+{
+    m_newFileInfo.setFile(m_path);
+}
 
 bool FileInfo::setPath(const QString &path) {
     if(m_path == path) {
@@ -12,8 +12,40 @@ bool FileInfo::setPath(const QString &path) {
     }
 
     m_path = path;
+    m_newFileInfo.setFile(m_path);
 
     update();
 
     return true;
+}
+
+bool FileInfo::isExist() const {
+    return (m_condition == Condition::FILE_EXIST || m_condition == Condition::FILE_CHANGED);
+}
+
+bool FileInfo::update() {
+    m_newFileInfo.refresh();
+
+    if(isExist()) {
+        if(m_newFileInfo.exists()) {
+            if(m_size == m_newFileInfo.size())
+                return false;
+
+            m_size = m_newFileInfo.size();
+            m_condition = Condition::FILE_CHANGED;
+            return true;
+        } else {
+            m_condition = Condition::FILE_NOT_EXIST;
+            return true;
+        }
+    } else {
+        if(m_newFileInfo.exists()) {
+            m_condition = Condition::FILE_EXIST;
+            return true;
+        }
+
+        return false;
+    }
+
+    return false;
 }
--- fileinfo.h
+++ fileinfo.h
@@ -1,6 +1,8 @@
 #ifndef FILEINFO_H
 #define FILEINFO_H
 
+#include <QFileInfo>
+
 #include "iFileInfo.h"
 
 class FileInfo : public IFileInfo {
@@ -10,18 +12,20 @@ public:
 
     QString getPath() const override {return m_path;}
     qint64 getSize() const  override {return m_size;}
-    Condition getCondition() const override {return Condition::NONE;}
+    Condition getCondition() const override {return m_condition;}
 
     bool setPath(const QString &path) override;
 
-    bool isExist() const override {return m_isExist;}
+    bool isExist() const override;
 
-    bool update() override {return false;}
+    bool update() override;
 
 private:
     QString m_path = "";
     qint64 m_size = 0;
-    bool m_isExist = false;
+    Condition m_condition = NONE;
+
+    QFileInfo m_newFileInfo;
 };
 
 #endif // FILEINFO_H
